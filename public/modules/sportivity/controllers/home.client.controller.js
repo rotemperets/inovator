05537030b4178e42a8f0a1624dfa3b48147fcb86
ofir fridman <OFridman@dgit.com>
2014-06-24 17:42:44 +0300

    datePicker to New Activity view

--- public/modules/sportivity/controllers/home.client.controller.js
+++ public/modules/sportivity/controllers/home.client.controller.js
@@ -11,12 +11,23 @@ angular.module('sportivity').controller('HomeController', ['$scope', '$location'
       $location.path(path + '/' + activityId);
     };
 
-		$scope.getClass = function(activity){
+		$scope.getActivitiesClass = function(activity){
 			var clazz = {'otherInventionsList':true};
 			if(activity.user._id == $scope.authentication.user._id){
 				clazz = {'myInventionsList':true}
 			}
 			return clazz;
 		};
+
+		$scope.getGroupsClass = function(group){
+			var clazz = {'otherInventionsList':true};
+			if(group.user._id == $scope.authentication.user._id){
+				clazz = {'myGroupsHome':true}
+			}
+			return clazz;
+		};
   }
+
+
+
 ]);
