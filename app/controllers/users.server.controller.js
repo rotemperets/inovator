44e67f5e70e3977ec6d6b653123a3556abe70431
Rotem Perets <rotem.perets@sizmek.com>
2014-06-25 13:59:50 +0300

    Change smtp

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -16,8 +16,8 @@ var transport = nodemailer.createTransport();
 var transport = nodemailer.createTransport('SMTP', {
     service: 'Gmail',
     auth: {
-        user: 'maorfrank@gmail.com',
-        pass: 'bimba1234'
+        user: 'sportivityrotem@gmail.com',
+        pass: 'Sp0rtivity'
     }
 });
 
